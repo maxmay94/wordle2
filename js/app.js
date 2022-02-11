3709baf0869e98b20029bfdc708ba654834fb269
maxmay94 <maxmay94@gmail.com>
2022-02-10 19:15:14 -0500

    start on JavaScript

--- js/app.js
+++ js/app.js
@@ -4,23 +4,69 @@
 
 /*---------------------------- Variables (state) ----------------------------*/
 
-
+let turnNum = 0
+let currentGuess = []
+let prevTurns = []
+let secretWord = 'bread'
 
 /*------------------------ Cached Element References ------------------------*/
 
 const squares = document.querySelectorAll('.square')
-const keys = document.querySelectorAll('.key')
+// const keys = document.querySelectorAll('.key')
+
 const gameBoard = document.querySelector('#main-game-area')
-const mainTitle = document.getElementById('main-title')
+const keyBoard = document.querySelector('#keyboard')
 
 /*----------------------------- Event Listeners -----------------------------*/
 
 gameBoard.addEventListener('click', handleClick)
+keyBoard.addEventListener('click', handleClick)
 
 /*-------------------------------- Functions --------------------------------*/
 
-
 function handleClick(evt) {
-  
-  console.log(evt.target)
+  let keyClick = evt.target.id
+  if(evt.target.id === 'delete') {
+    renderDelete()
+  } else if (evt.target.id === 'enter') {
+    checkGuess()
+  } else if (evt.target.className === 'key') {
+    renderKey(keyClick)
+  } else if (evt.target.className === 'square') {
+    renderBox(keyClick)
+  }
+}
+
+function renderKey(keyClick) {
+  if (currentGuess.length < 5) {
+    squares[(prevTurns.length * 5) + (currentGuess.length)].textContent = keyClick
+    currentGuess.push(keyClick)
+  } else {
+    console.log('2Much')
+  }
+}
+
+function renderDelete() {
+  currentGuess.pop()
+  squares[(prevTurns.length * 5) + (currentGuess.length)].textContent = ''
+  return
+}
+
+function renderEnter() {
+
+}
+
+function renderBox(keyClick) {
+  console.log(keyClick)
+  //come back to this
+  //make it so user can edit a letter without deleting previous letters
+}
+
+function checkGuess() {
+  if(currentGuess.length === 5) {
+    
+  }
+  else {
+    return
+  }
 }
