c2414b794eda6f259411122838091616d1338abd
maxmay94 <maxmay94@gmail.com>
2022-02-11 19:22:35 -0500

    last push tonight, fix some logic

--- js/app.js
+++ js/app.js
@@ -27,6 +27,7 @@ keyBoard.addEventListener('click', handleClick)
 
 function handleClick(evt) {
   let keyClick = evt.target.id
+  clearAnimation()
   if (keyClick === 'delete') {
     renderDelete()
   } else if (keyClick === 'enter') {
@@ -58,34 +59,56 @@ function renderBox(keyClick) {
 }
 
 function checkGuess() {
-  let curRow = (prevTurns.length * 5)
   let thisTurn = []
+  let check = currentGuess.join('').toLocaleLowerCase()
 
   if (currentGuess.length === 5) {
-    currentGuess.forEach((letter, i) => {
-      if (letter === secretWord[i]) {
-        thisTurn.push('correct')
-        let idx = getKeyIndex(letter)
-        keys[idx].classList.add('correct')
-      } else if (secretWord.includes(letter)) {  // Update for edge cases // ex. only color one 'A' in 'PANDA' if secret word is 'BREAD'
-        thisTurn.push('almost')
-        let idx = getKeyIndex(letter)
-        keys[idx].classList.add('almost')
-      } else {
-        thisTurn.push('miss')
-        let idx = getKeyIndex(letter)
-        keys[idx].classList.add('miss')
-      }    
-    })
-    turnNum++
-    prevTurns.push(currentGuess) // maybe push thisTurn?? haven't decided yet. might make it easier to make a share emoji thing at the end
-    renderGuess(thisTurn) 
-    currentGuess = []
-    return
-  }
-  else {
-    return
-  }
+    if(checkWord(check)) { 
+      currentGuess.forEach((letter, i) => {
+
+        if (letter === secretWord[i]) {
+          thisTurn.push('correct')
+          let idx = getKeyIndex(letter)
+          keys[idx].classList.add('correct')
+        } else if (secretWord.includes(letter)) {  // Update for edge cases // ex. only color one 'A' in 'PANDA' if secret word is 'BREAD'
+          thisTurn.push('almost')
+          let idx = getKeyIndex(letter)
+          keys[idx].classList.add('almost')
+        } else {
+          thisTurn.push('miss')
+          let idx = getKeyIndex(letter)
+          keys[idx].classList.add('miss')
+        }    
+      })
+      turnNum++
+      prevTurns.push(currentGuess) // maybe push thisTurn?? haven't decided yet. might make it easier to make a share emoji thing at the end
+      renderGuess(thisTurn) 
+      currentGuess = []
+      return
+    }
+    else {
+      renderWrong()
+      return
+    }
+  } 
+}
+
+function clearAnimation() {
+  r0.classList.remove('animate__animated', 'animate__shakeX')
+  r1.classList.remove('animate__animated', 'animate__shakeX')
+  r2.classList.remove('animate__animated', 'animate__shakeX')
+  r3.classList.remove('animate__animated', 'animate__shakeX')
+  r4.classList.remove('animate__animated', 'animate__shakeX')
+  r5.classList.remove('animate__animated', 'animate__shakeX')
+}
+
+function renderWrong() {
+  if(prevTurns.length === 0) r0.classList.add('animate__animated', 'animate__shakeX')
+  if(prevTurns.length === 1) r1.classList.add('animate__animated', 'animate__shakeX')
+  if(prevTurns.length === 2) r2.classList.add('animate__animated', 'animate__shakeX')
+  if(prevTurns.length === 3) r3.classList.add('animate__animated', 'animate__shakeX')
+  if(prevTurns.length === 4) r4.classList.add('animate__animated', 'animate__shakeX')
+  if(prevTurns.length === 5) r5.classList.add('animate__animated', 'animate__shakeX')
 }
 
 function getKeyIndex(char) {
@@ -98,6 +121,9 @@ function getKeyIndex(char) {
 
 function renderGuess(thisTurn) {
   let i = 0 
+  squares[((prevTurns.length - 1) * 5) + i].classList.add('animate__animated', 'animate__flip', `${thisTurn[i]}`, 'shadow')
+  i++
+  //Do once first so it happens right on click instaed of waiting a second
   let timer = setInterval(function () {
     squares[((prevTurns.length - 1) * 5) + i].classList.add('animate__animated', 'animate__flip', `${thisTurn[i]}`, 'shadow')
     i++
